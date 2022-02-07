6696744a1313f45a9aeef6bd16664d1432e26bdf
Luke <shijiluke@gmail.com>
2022-02-08 07:54:39 +0800

    2 8 740

---
+++ downloadCtn.py
@@ -0,0 +1,14 @@
+import datetime
+import urllib.request
+
+dateString = (datetime.datetime.today() - datetime.timedelta(days=1)).strftime('%Y%m%d')
+
+ctnWeb = "https://www.chp.gov.hk/files/pdf/ctn_" + \
+         dateString + \
+         ".pdf"
+
+response = urllib.request.urlopen(ctnWeb)
+print(response)
+file = open('ctn', 'wb')
+file.write(response.read())
+file.close()
