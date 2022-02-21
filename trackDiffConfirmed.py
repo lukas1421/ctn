bcd207a8e41dbb21266c6cf4bba29f5c467f080f
Luke <shijiluke@gmail.com>
2022-02-21 09:47:39 +0800

    2 21 947

--- trackDiffConfirmed.py
+++ trackDiffConfirmed.py
@@ -1,39 +1,68 @@
-import re
-
 from methods import getQuarantineDict
 
 regionEstatesT = getQuarantineDict('file_confirmedBuildings_T')
 regionEstatesY = getQuarantineDict('file_confirmedBuildings_T-1')
-diff = {}
+increases = {}
 
+# INCREASE
 for r in regionEstatesT:
     if r not in regionEstatesY:
-        # print("new region:", r)
-        diff[r] = regionEstatesT[r]
+        increases[r] = regionEstatesT[r]
     else:
         for e in regionEstatesT[r]:
 
             if e not in regionEstatesY[r]:
-                if r not in diff:
-                    diff[r] = {}
-                diff[r][e] = regionEstatesT[r][e]
-            #   print("new estate:", r, e, regionEstatesT[r][e])
+                if r not in increases:
+                    increases[r] = {}
+                increases[r][e] = regionEstatesT[r][e]
             else:
                 for b in regionEstatesT[r][e]:
                     if b not in regionEstatesY[r][e]:
-                        if r not in diff:
-                            diff[r] = {}
-                        if e not in diff[r]:
-                            diff[r][e] = []
-                        diff[r][e].append(b)
-            #            print("new building", r, e, b)
-print(diff)
+                        if r not in increases:
+                            increases[r] = {}
+                        if e not in increases[r]:
+                            increases[r][e] = []
+                        increases[r][e].append(b)
+print(increases)
 
-print("************确诊不同****************")
-for r in sorted(diff, key=lambda r: (sum(len(diff[r][e]) for e in diff[r].keys())), reverse=True):
-    totalEstateInRegion = sum(1 for e in diff[r].keys())
-    totalBuildingsInRegion = sum(len(diff[r][e]) for e in diff[r].keys())
+# DECREASE
+decreases={}
+for r in regionEstatesY:
+    if r not in regionEstatesT:
+        decreases[r] = regionEstatesY[r]
+    else:
+        for e in regionEstatesY[r]:
+            if e not in regionEstatesT[r]:
+                if r not in decreases:
+                    decreases[r] = {}
+                decreases[r][e] = regionEstatesY[r][e]
+            else:
+                for b in regionEstatesY[r][e]:
+                    if b not in regionEstatesT[r][e]:
+                        if r not in decreases:
+                            decreases[r] = {}
+                        if e not in decreases[r]:
+                            decreases[r][e] = []
+                        decreases[r][e].append(b)
+print(decreases)
+
+
+print("************确诊增加****************")
+for r in sorted(increases, key=lambda r: (sum(len(increases[r][e]) for e in increases[r].keys())), reverse=True):
+    totalEstateInRegion = sum(1 for e in increases[r].keys())
+    totalBuildingsInRegion = sum(len(increases[r][e]) for e in increases[r].keys())
     print("***************", r, "****************", "Estates:", totalEstateInRegion
           , "Bldg:", totalBuildingsInRegion)
-    for k in sorted(diff[r], key=lambda k: len(diff[r][k]), reverse=True):
-        print(k, diff[r][k], len(diff[r][k]))
+    for k in sorted(increases[r], key=lambda k: len(increases[r][k]), reverse=True):
+        print(k, increases[r][k], len(increases[r][k]))
+
+print("                                                                        ")
+print("************************************************************************")
+print("************确诊减少****************")
+for r in sorted(decreases, key=lambda r: (sum(len(decreases[r][e]) for e in decreases[r].keys())), reverse=True):
+    totalEstateInRegion = sum(1 for e in decreases[r].keys())
+    totalBuildingsInRegion = sum(len(decreases[r][e]) for e in decreases[r].keys())
+    print("***************", r, "****************", "Estates:", totalEstateInRegion
+          , "Bldg:", totalBuildingsInRegion)
+    for k in sorted(decreases[r], key=lambda k: len(decreases[r][k]), reverse=True):
+        print(k, decreases[r][k], len(decreases[r][k]))
